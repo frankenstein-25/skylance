621873e861de862929d846310a407379f48288c9
frankenstein-25 <bhadaniahh@gmail.com>
2023-08-13 15:09:33 +0530

    hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,14 +9,18 @@ module.exports = {
                 },
                 'secondary': {
                     DEFAULT: '#04122d',
+                    2:'#16223A'
                 },
                 "dark": {
                     DEFAULT: "#4A5168",
                     2: '#172138'
                 },
-                "light":{
-                    DEFAULT:"#c381db0f",
-
+                "light": {
+                    DEFAULT: "#c381db0f",
+                    2:'#F6F8FE'
+                },
+                "green": {
+                    DEFAULT: '#09C356'
                 }
             },
         },
